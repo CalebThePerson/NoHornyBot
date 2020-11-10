6e7230f4c751a1b3248a29f18d1dff5d3da7cb5d
CalebThePerson <Cayj15@gmail.com>
2020-11-10 01:28:46 -0800

    added non horny pictures

--- SourceCode/Scrapping/RedditScrap.py
+++ SourceCode/Scrapping/RedditScrap.py
@@ -4,7 +4,8 @@ import praw
 import urllib.request
 from RandomWordGenerator import RandomWord
 from PIL import Image
-
+import os
+import time
 '''
 remember to create a redditapi file with this 
 MyReddit = praw.Reddit(client_id="",
@@ -17,11 +18,11 @@ MyReddit = praw.Reddit(client_id="",
 
 #Getting The food images 
 #, "dankmemes", "goodanimememes"
-SafeSubs = ["FoodPorn","goodanimememes"]
-
+# SafeSubs = ["aww","dankmemes"]
+nonsafe = ["juicyasians","Naked"]
 UrlArray = []
 
-for sub in SafeSubs:
+for sub in nonsafe:
     subreddit = MyReddit.subreddit(sub)
 
 
@@ -29,7 +30,7 @@ for sub in SafeSubs:
     #             "id":[], 
     #             "url":[] }
 
-    for submission in subreddit.top(limit = 5):
+    for submission in subreddit.top(limit = 250):
         # topics_dict["title"].append(submission.title)
         # topics_dict["id"].append(submission.id)
         # topics_dict["url"].append(submission.url)
@@ -38,11 +39,25 @@ for sub in SafeSubs:
 print(UrlArray)
 
 R = RandomWord(max_word_size=5)
+started = time.time()
 
 for picture in UrlArray:
     NewName = R.generate()
-    os.chdir("/Users/calebtheperson/PythonProjects/NoHornyBot/SourceCode/NoHorny")
-    Thepicture = urllib.request.urlretrieve(picture, f"{NewName}.jpg")
+    os.chdir("/home/pi/Desktop/Code/NoHornyBot/SourceCode/Horny")
+    # Thepicture = urllib.request.urlretrieve(picture, f"{NewName}.jpg")
+    if ".jpg"or ".png" in picture:
+        try:
+            ThePicture = requests.get(picture)
+            with open(f"{NewName}.png","wb") as file:
+                file.write(ThePicture.content)
+                file.close()
+        except requests.exceptions.ConnectionError:
+            print("I must wait weeb")
+            time.sleep(120)
+        except requests.exceptions.MissingSchema:
+            pass
     # image = Image.open(f"{NewName}.jpg")
 
     # image.save(f"/NoHorny/{NewName}.jpg", )
+print("done")
+print(f"It took about {time.time() - started}")
---
+++ SourceCode/Scrapping/redditapi.py
@@ -0,0 +1,7 @@
+import praw
+
+MyReddit = MyReddit = praw.Reddit(client_id="-FJjR4uid45Yhw",
+                     client_secret="ZVB9j9lNZbt96YUImn2sf0mBU1vvPQ",  #your client secret
+                     user_agent="ScrappingStuff", #user agent name
+                     username = "CalebTheHokage",
+                     password = "caleb74290") 
